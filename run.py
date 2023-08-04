0f67c75e0c7613500a7392054eaf93f8c004505d
BLACK-143 <126906227+NORMALUSER-143@users.noreply.github.com>
2023-08-05 01:33:40 +0600

    Update run.py

--- run.py
+++ run.py
@@ -1,3 +1,16 @@
-chmod 777 rand
+#UTF-8
+from os import system as cmd
+from time import sleep as beep
+import platform
+bit = platform.architecture()[0]
+cmd('git pull');cmd('clear')
 
-./rand
+if bit=='64bit':
+    print('[!] 64 Bit Device');beep(2)
+    cmd('chmod 777 rand')
+    cmd('./rand')
+
+
+else:
+    print('Your Device did not support this tool...!!')
+    exit()
